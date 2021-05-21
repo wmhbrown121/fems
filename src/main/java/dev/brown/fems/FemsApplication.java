0c4e9379ba10e6ec34e1110484db870a6c6b2c91
Brown <2014719@cognizant.com>
2021-05-21 15:28:36 -0500

    initial commit

---
+++ src/main/java/dev/brown/fems/FemsApplication.java
@@ -0,0 +1,13 @@
+package dev.brown.fems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/brown/fems/FemsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.brown.fems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
